083dfcb2705f9e0e6f18206e140c9b6899a58b5c
王飞 <20907200@qq.com>
2015-11-27 12:33:31 +0800

    create project

---
+++ hisales/hisales/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  hisales
+//
+//  Created by 王飞 on 15/11/27.
+//  Copyright (c) 2015年 edaysoft.cn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
